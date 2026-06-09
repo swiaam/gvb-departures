3bc9ee905d576886620932caf24558a9ba0db064
swiaam <info@kanoon.food>
2026-06-09 23:24:46 +0200

    Add service worker for instant updates on iOS PWA

---
+++ docs/sw.js
@@ -0,0 +1,31 @@
+const VERSION = 'v1';
+const CACHE = 'gvb-' + VERSION;
+
+const PRECACHE = [
+  '/gvb-departures/gvb-departures.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(PRECACHE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// Network-first for HTML so updates are picked up immediately
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request)
+        .then(res => { caches.open(CACHE).then(c => c.put(e.request, res.clone())); return res; })
+        .catch(() => caches.match(e.request))
+    );
+  }
+});
